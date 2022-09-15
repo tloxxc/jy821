6215c8fa1aaa1dd5af74b411291107386d4667ba
xc <102371032+tloxxc@users.noreply.github.com>
2022-09-15 16:54:25 +0800

    Update DefaultConfig.java

--- app/src/main/java/com/github/tvbox/osc/util/DefaultConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/DefaultConfig.java
@@ -48,7 +48,7 @@ public class DefaultConfig {
             }
         }
         if (withMy)
-            data.add(0, new MovieSort.SortData("my0", "主页"));
+            data.add(0, new MovieSort.SortData("my0", "换源"));
         Collections.sort(data);
         return data;
     }
